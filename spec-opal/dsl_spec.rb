c9b8afd5a79dd704fc3bb9e37e0d5bcaf23a0598
Andy Maleh <andy.am@gmail.com>
2020-07-03 12:54:07 -0400

    Passing spec for combo data-binding

--- spec-opal/dsl_spec.rb
+++ spec-opal/dsl_spec.rb
@@ -8,6 +8,25 @@ module GlimmerSpec
     
     let(:title) {'Hello, World!'}
     
+    before :all do
+      class Person
+        attr_accessor :country, :country_options
+        
+        def initialize
+          self.country_options=["", "Canada", "US", "Mexico"]
+          self.country = "Canada"
+        end
+      
+        def reset_country
+          self.country = "Canada"
+        end
+      end    
+    end
+    
+    after :all do
+      GlimmerSpec.send(:remove_const, :Person) if GlimmerSpec.const_defined?(:Person)
+    end
+    
     after do
       Glimmer::SWT::WidgetProxy.reset_max_id_numbers!
       @target.dispose if @target.respond_to?(:dispose)
@@ -37,20 +56,51 @@ module GlimmerSpec
       expect(label_element.inner_html).to eq(title)
     end
     
-    it 'renders shell with composite containing combo' do
+    it 'renders shell with composite containing combo (read only)' do
       @target = shell {
         composite {
-          combo {
+          combo(:read_only) {
           }
         }
       }
       
       composite_element = $document.css('body > div#shell-1.shell > div#composite-1.composite').first
       expect(composite_element).to be_a(Browser::DOM::Element)
-      combo_element = $document.css('body > div#shell-1.shell > div#composite-1.composite > select#combo-1').first
+      combo_element = $document.css('body > div#shell-1.shell > div#composite-1.composite > select#combo-1.combo').first
       expect(combo_element).to be_a(Browser::DOM::Element::Select)
     end
     
-    it 'renders shell with composite containing combo (read only)'
+    it 'renders shell with composite containing data-bound combo (read only)' do
+      person = Person.new
+      @target = shell {
+        composite {
+          combo(:read_only) {
+            selection bind(person, :country)
+          }
+        }
+      }
+      
+      composite_element = $document.css('body > div#shell-1.shell > div#composite-1.composite').first
+      expect(composite_element).to be_a(Browser::DOM::Element)
+      
+      combo_element = $document.css('body > div#shell-1.shell > div#composite-1.composite > select#combo-1.combo').first
+      expect(combo_element).to be_a(Browser::DOM::Element::Select)
+      
+      combo_option_element1 = $document.css('body > div#shell-1.shell > div#composite-1.composite > select#combo-1.combo > option:nth-child(1)').first
+      expect(combo_option_element1).to be_a(Browser::DOM::Element)
+      expect(combo_option_element1.inner_html).to eq('')
+      
+      combo_option_element2 = $document.css('body > div#shell-1.shell > div#composite-1.composite > select#combo-1.combo > option:nth-child(2)').first
+      expect(combo_option_element2).to be_a(Browser::DOM::Element)
+      expect(combo_option_element2.inner_html).to eq('Canada')
+      
+      combo_option_element3 = $document.css('body > div#shell-1.shell > div#composite-1.composite > select#combo-1.combo > option:nth-child(3)').first
+      expect(combo_option_element3).to be_a(Browser::DOM::Element)
+      expect(combo_option_element3.inner_html).to eq('US')
+      
+      combo_option_element4 = $document.css('body > div#shell-1.shell > div#composite-1.composite > select#combo-1.combo > option:nth-child(4)').first
+      expect(combo_option_element4).to be_a(Browser::DOM::Element)
+      expect(combo_option_element4.inner_html).to eq('Mexico')
+    end
   end
 end
